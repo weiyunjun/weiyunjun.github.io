61c41707b7fca118dd309f3b9e36723da5aab290
weiyunjun <shanmoweiyunjun@gmail.com>
2026-08-22 00:07:18 +0800

    fix: sort posts by pubDate instead of modDate

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -21,19 +21,12 @@ export async function getCategories() {
   return categories
 }
 
-export async function getPosts(isArchivePage = false) {
+export async function getPosts() {
   const posts = await getCollection('posts')
 
-  posts.sort((a, b) => {
-    if (isArchivePage) {
-      return dayjs(a.data.pubDate).isBefore(dayjs(b.data.pubDate)) ? 1 : -1
-    }
-
-    const aDate = a.data.modDate ? dayjs(a.data.modDate) : dayjs(a.data.pubDate)
-    const bDate = b.data.modDate ? dayjs(b.data.modDate) : dayjs(b.data.pubDate)
-
-    return aDate.isBefore(bDate) ? 1 : -1
-  })
+  posts.sort((a, b) =>
+    dayjs(b.data.pubDate).valueOf() - dayjs(a.data.pubDate).valueOf(),
+  )
 
   if (import.meta.env.PROD) {
     return posts.filter(post => post.data.draft !== true)
